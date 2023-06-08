12920e5c8aac45f44d1b1fc4051a02e31086cd42
alex <alex.labra.94@gmail.com>
2023-06-08 15:14:28 -0700

    update name endpoint

--- index.js
+++ index.js
@@ -174,6 +174,39 @@ app.post('/delete-class', async (req, res) => {
     }
 });
 
+//SETTINGS: update name
+app.post('/update-name', async (req, res) => {
+    const { email, password, newFirstName, newLastName } = req.body;
+
+    if (!email || !password || !newFirstName || !newLastName) {
+        return res.status(400).send('Email, password, first name, and last name are required.');
+    }
+
+    try {
+        // Check if the user exists
+        const [userResults] = await pool2.query('SELECT * FROM users WHERE email = ?', [email]);
+        const user = userResults[0];
+
+        if (!user) {
+            return res.status(404).send('User not found.');
+        }
+
+        // Check if the password matches
+        if (user.password !== password) {
+            return res.status(401).send('Invalid password.');
+        }
+
+        // Update the user's first name and last name
+        await pool2.query('UPDATE users SET first_name = ?, last_name = ? WHERE email = ?', [newFirstName, newLastName, email]);
+
+        res.status(200).send('Name updated successfully.');
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error');
+    }
+});
+
+
 // Endpoint to send newUser Data to the DataBase
 app.post('/newUser', (req, res) => {
     const userID = req.body.userID;
