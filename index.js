686afba1f81a76135d4855653b0b7b80850f9e38
alex <alex.labra.94@gmail.com>
2023-07-10 14:34:53 -0700

    added Student list endpoint

--- index.js
+++ index.js
@@ -117,6 +117,43 @@ app.use('/newUser', newUserRoute);
 const emailServiceRoute = require('./routes/emailService');
 app.use('/emailService', emailServiceRoute);
 
+// Retrieve student list
+app.post('/retrieve-students', async (req, res) => {
+    const { classIDs } = req.body;
+    console.log(classIDs)
+    try {
+        // Retrieve the students enrolled in the classes, including their class names
+        const queryString = `SELECT u.userID, u.first_name, u.last_name, u.email, c.className
+            FROM users u
+            INNER JOIN user_classes uc ON u.userID = uc.userID
+            INNER JOIN classes c ON uc.classID = c.classID
+            WHERE uc.classID IN (?)
+            AND u.type <> 'teacher'`;
+
+        const [results] = await pool2.query(queryString, [classIDs]);
+
+        // Restructure the response object with class names as keys
+        const studentsByClass = {};
+
+        results.forEach((student) => {
+            const { className, ...studentData } = student;
+
+            if (!studentsByClass[className]) {
+                studentsByClass[className] = [];
+            }
+
+            studentsByClass[className].push(studentData);
+        });
+
+        // Send the student list in the response
+        res.status(200).json(studentsByClass);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
 });
